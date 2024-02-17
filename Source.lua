634be505e259a91359aaa54961de800c6b09e439
GhostQut <69810133+GhostQut@users.noreply.github.com>
2024-02-17 13:59:40 +0300

    2D

--- Source.lua
+++ Source.lua
@@ -1291,17 +1291,17 @@ parent.TopBar.ProfileMenu.PlayerProfile.TextLabel.Text = game:GetService("Player
 parent.TopBar.ProfileMenu.PlayerProfile.ImageLabel.Image = game:GetService("Players"):GetUserThumbnailAsync(game.Players.LocalPlayer.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size100x100)
 
 local functionCheck = false
+for i, v in pairs(game.CoreGui:GetChildren()) do
+	if v.Name == DrRay then
+		game.CoreGui.DrRay:Destroy()
+		functionCheck = true
+	else
+		functionCheck = false
+		return
+	end
+end)
 
 function UILIB:Load(name, img, direction)
-	for i, v in pairs(game.CoreGui:GetChildren()) do
-	    if v.Name == DrRay then
-			game.CoreGui.DrRay:Destroy()
-			functionCheck = true
-		else
-			functionCheck = false
-			return
-		end
-	end)
 	local self = setmetatable({}, UILIB)
 	task.spawn(function()
 			local tw = twServ:Create(parent.MainBar, TweenInfo.new(0.4, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), { Position = UDim2.new(0.23, 0,0.212, 0) })
